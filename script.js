27bcf72a1141e63abe819a278aec8e9f9847ce13
OUSStoic <153828336+OUSStoic@users.noreply.github.com>
2024-01-05 22:55:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -23,18 +23,18 @@ function staticLoadPlaces() {
 var models = [
     {
         url: './assets/ali/scene.gltf',
-        scale: '0.5 0.5 0.5',
+        scale: '0.08 0.05 0.05',
         info: 'solider, Lv. 5, HP 10/10',
         rotation: '0 180 0',
     },
     {
-        url: './assets/articuno/scene.gltf',
+        url: './assets/cyper/scene.gltf',
         scale: '0.2 0.2 0.2',
         rotation: '0 180 0',
         info: 'Articuno, Lv. 80, HP 100/100',
     },
     {
-        url: './assets/dragonite/scene.gltf',
+        url: './assets/robot/scene.gltf',
         scale: '0.08 0.08 0.08',
         rotation: '0 180 0',
         info: 'Dragonite, Lv. 99, HP 150/150',
